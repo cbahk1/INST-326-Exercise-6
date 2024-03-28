2b8bb6f0f0d6c991940de48b8846d84ae52b2611
Christian Bahk <christianbahk@wireless-10-104-116-214.umd.edu>
2024-03-28 19:55:17 -0400

    Final commit

--- Exercise6.py
+++ Exercise6.py
@@ -6,3 +6,7 @@ print('Editing in github so I can pull the changes')
 print('Turns out I dont pull it I merge to get it on my client')
 
 #cool it also shows what i changed with a green line 
+
+print('Some more practice for git add push . git commit -m "message" git merge')
+
+print('Goodbye')
