a7794c1e67e6107e5724b9108621cc780099fc81
Christian Bahk <christianbahk@wireless-10-104-116-214.umd.edu>
2024-03-28 19:53:46 -0400

    new changes

--- Exercise6.py
+++ Exercise6.py
@@ -1,3 +1,8 @@
 print('Hello World!')
 
 print('Editing in github so I can pull the changes')
+
+
+print('Turns out I dont pull it I merge to get it on my client')
+
+#cool it also shows what i changed with a green line 
